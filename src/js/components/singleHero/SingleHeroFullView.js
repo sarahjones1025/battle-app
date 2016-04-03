0ed191366e318213c08d8e822753b3f4ca8d1b57
Fabian <murillo.a.fabian@gmail.com>
2016-04-03 16:28:43 -0400

    fixed merge issue

--- src/js/components/singleHero/SingleHeroFullView.js
+++ src/js/components/singleHero/SingleHeroFullView.js
@@ -31,19 +31,16 @@ var SingleHeroFullView = Backbone.View.extend({
 
     render: function () {
          //The following is the full path of the image.
-<<<<<<< HEAD
-=======
-         var stats = utils.getStats(this.model.get('id'));
 
-         var strength = parseInt(stats.strength)*(14.3) + '%';
-         var speed = parseInt(stats.speed)*(14.3) + '%';
-         var fighting = parseInt(stats.fighting)*(14.3) + '%';
-         var durability = parseInt(stats.durability)*(14.3) + '%';
-         var intelligence = parseInt(stats.intelligence)*(14.3) + '%';
-         var energy = parseInt(stats.energy)*(14.3) + '%';
+         // var stats = utils.getStats(this.model.get('id'));
 
+         // var strength = parseInt(stats.strength)*(14.3) + '%';
+         // var speed = parseInt(stats.speed)*(14.3) + '%';
+         // var fighting = parseInt(stats.fighting)*(14.3) + '%';
+         // var durability = parseInt(stats.durability)*(14.3) + '%';
+         // var intelligence = parseInt(stats.intelligence)*(14.3) + '%';
+         // var energy = parseInt(stats.energy)*(14.3) + '%';
 
->>>>>>> working
 
         this.$el.find('.character_bio img').attr('src',
              (this.model.get('thumbnail')
@@ -54,16 +51,16 @@ var SingleHeroFullView = Backbone.View.extend({
              (this.model.get('thumbnail')
              + '.' + this.model.get('extension')));
 
-        this.$el.find('.description p').html(this.model.get('description'));
-        this.$el.find('.combatant h1').html(this.model.get('name'));
-        this.$el.find('.character_bio h1').html($('<span>').html(this.model.get('name').split('').splice(0, 1).join()));
-        this.$el.find('.character_bio h1').append(this.model.get('name').split('').splice(1).join(''));
-        this.$el.find('.strength > div').css({'width': strength});
-        this.$el.find('.speed > div').css({'width': speed});
-        this.$el.find('.fighting > div').css({'width': fighting});
-        this.$el.find('.durability > div').css({'width': durability});
-        this.$el.find('.intelligence > div').css({'width': intelligence});
-        this.$el.find('.energy > div').css({'width': energy});
+        // this.$el.find('.description p').html(this.model.get('description'));
+        // this.$el.find('.combatant h1').html(this.model.get('name'));
+        // this.$el.find('.character_bio h1').html($('<span>').html(this.model.get('name').split('').splice(0, 1).join()));
+        // this.$el.find('.character_bio h1').append(this.model.get('name').split('').splice(1).join(''));
+        // this.$el.find('.strength > div').css({'width': strength});
+        // this.$el.find('.speed > div').css({'width': speed});
+        // this.$el.find('.fighting > div').css({'width': fighting});
+        // this.$el.find('.durability > div').css({'width': durability});
+        // this.$el.find('.intelligence > div').css({'width': intelligence});
+        // this.$el.find('.energy > div').css({'width': energy});
 
 
     }
