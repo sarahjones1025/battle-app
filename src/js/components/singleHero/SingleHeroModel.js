1ff9e9e74c642e8782768bbdb9de8272e8386488
David Millican <dave_millican@yahoo.com>
2016-04-04 14:06:42 -0400

    saved apiKey

--- src/js/components/singleHero/SingleHeroModel.js
+++ src/js/components/singleHero/SingleHeroModel.js
@@ -10,6 +10,8 @@ var SingleHeroModel = Backbone.Model.extend({
 
     initialize: function () {
         this.url = function () {
+            
+            //var marvelKey = 'apikey=cd80e84f4acc3f0d2cdabd391244ab24';
 
             var marvelKey = 'apikey=cd80e84f4acc3f0d2cdabd391244ab24';
 
